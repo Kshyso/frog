4b5ac9be4f517a4802d2e66a51bf23e01f51508d
Kshysio <Kshysio@protonmail.com>
2023-09-27 20:06:00 +0200

    more realistic scale

--- character.gd
+++ character.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 
 const SPEED := 5.0
-const JUMP_VELOCITY := 4.5
+const JUMP_VELOCITY := 2.0
 const LOOK_SENS := 0.4
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
