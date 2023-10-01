090f1456217e484fee77e7d5aee00b231bc6caf0
Kshysio <Kshysio@protonmail.com>
2023-10-01 22:37:21 +0200

    Improved mouse capture

--- character.gd
+++ character.gd
@@ -11,18 +11,23 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
+func _camera_rotation(event: InputEvent):
+	rotate_y(deg_to_rad(-event.relative.x * LOOK_SENS))
+	$Neck.rotate_x((deg_to_rad(-event.relative.y * LOOK_SENS)))
+	$Neck.rotation_degrees.x = clamp(($Neck.rotation_degrees.x), -90, 90)
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -34,7 +39,9 @@ func _physics_process(delta):
 	move_and_slide()
 
 func _input(event):
-	if event is InputEventMouseMotion:
-		rotate_y(deg_to_rad(-event.relative.x * LOOK_SENS))
-		$Neck.rotate_x((deg_to_rad(-event.relative.y * LOOK_SENS)))
-		$Neck.rotation_degrees.x = clamp(($Neck.rotation_degrees.x), -70, 70)
+	if Input.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	elif Input.is_action_pressed("mouse_left"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		_camera_rotation(event)
