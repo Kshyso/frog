851999a5188073e56cbf7ca3d30e615a448d3171
Kshysio <Kshysio@protonmail.com>
2023-10-05 22:09:00 +0200

    Added fridge

--- character.gd
+++ character.gd
@@ -1,9 +1,9 @@
 extends CharacterBody3D
 
 
-const SPEED := 5.0
-const JUMP_VELOCITY := 2.0
-const LOOK_SENS := 0.4
+@export var speed := 5.0
+@export var jump_velocity := 2.5
+@export var look_sens := 0.4
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -12,8 +12,8 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _camera_rotation(event: InputEvent):
-	rotate_y(deg_to_rad(-event.relative.x * LOOK_SENS))
-	$Neck.rotate_x((deg_to_rad(-event.relative.y * LOOK_SENS)))
+	rotate_y(deg_to_rad(-event.relative.x * look_sens))
+	$Neck.rotate_x((deg_to_rad(-event.relative.y * look_sens)))
 	$Neck.rotation_degrees.x = clamp(($Neck.rotation_degrees.x), -90, 90)
 
 func _physics_process(delta):
@@ -23,18 +23,18 @@ func _physics_process(delta):
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
 
