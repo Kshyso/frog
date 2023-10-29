520a35e3b978be27920a75e1880e7fc81a419118
Kshysio <Kshysio@protonmail.com>
2023-10-29 12:36:36 +0100

    Added dithering and stuff

--- scripts/character.gd
+++ scripts/character.gd
@@ -13,27 +13,34 @@ extends CharacterBody3D
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+var input_dir : Vector2
+var direction : Vector3
+var jumping : bool
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-func _camera_rotation(event: InputEvent):
+func camera_rotation(event: InputEvent):
 	rotate_y(deg_to_rad(-event.relative.x * look_sens))
 	$Neck.rotate_x((deg_to_rad(-event.relative.y * look_sens)))
 	$Neck.rotation_degrees.x = clamp(($Neck.rotation_degrees.x), -90, 90)
 
-func _physics_process(delta):
-	# Add the gravity.
+func camera_roll(delta):
+	var target_roll := -input_dir.x * max_roll_angle
+	rotation_degrees.z = lerp(rotation_degrees.z, target_roll, roll_speed * delta)
+
+func handle_input():
+	input_dir = Input.get_vector("left", "right", "forward", "backward")
+	direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		jumping = true
+	else:
+		jumping = false
+
+func movement(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_velocity
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("left", "right", "forward", "backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		if Input.is_action_pressed("sprint"):
 			velocity.x = move_toward(velocity.x, direction.x * max_sprint, acceleration)
@@ -44,9 +51,14 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, friction)
 		velocity.z = move_toward(velocity.z, 0, friction)
-	var target_roll := -input_dir.x * max_roll_angle
-	rotation_degrees.z = lerp(rotation_degrees.z, target_roll, roll_speed * delta)
+	if jumping == true:
+		velocity.y = jump_velocity
 	move_and_slide()
+
+func _physics_process(delta):
+	handle_input()
+	movement(delta)
+	camera_roll(delta)
 
 func _input(event):
 	if Input.is_action_pressed("ui_cancel"):
@@ -54,4 +66,4 @@ func _input(event):
 	elif Input.is_action_pressed("mouse_left"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		_camera_rotation(event)
+		camera_rotation(event)
