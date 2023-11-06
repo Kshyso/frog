270146e20ee5691627d3b8688c88aaee6775d412
Kshysio <Kshysio@protonmail.com>
2023-11-06 19:15:31 +0100

    I don't remember );

--- scripts/cashier.gd
+++ scripts/cashier.gd
@@ -1,6 +1,9 @@
 extends Sprite3D
+
+@onready var ChatBox := $"/root/Container/ChatBox"
+
 func _on_area_3d_body_entered(_body):
-	$ChatBox.show_chat("Elo elo, jest aplikacja żabka wariacie?")
+	ChatBox.show_chat("Elo elo, jest aplikacja zabka wariacie?")
 
 func _on_area_3d_body_exited(_body):
-	$ChatBox.hide_chat() # Replace with function body.
+	ChatBox.hide_chat() # Replace with function body.
